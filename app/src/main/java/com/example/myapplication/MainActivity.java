28434d041e34b3c37fd0afadd8d0841bfdec1cfc
donghyeon <kevink3@naver.com>
2019-12-03 21:24:18 +0900

    asdfgh

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,9 +3,14 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    EditText log;
+    EditText reg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,6 +19,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    
+    public void onclick(View v)
+    {
+        int id;
+        id = v.getId();
+        switch(id) {
+            case R.id.LoginBtn:
+
+
+                break;
+            case R.id.RegistBtn:
+
+
+
+                break;
+        }
+
+
+    }
+
 
 }
