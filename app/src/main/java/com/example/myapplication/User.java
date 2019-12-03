d57b5d653a9f48e32939f79b7ebef1608b52af72
donghyeon <kevink3@naver.com>
2019-12-03 20:39:44 +0900

    12345

--- app/src/main/java/com/example/myapplication/User.java
+++ app/src/main/java/com/example/myapplication/User.java
@@ -10,6 +10,25 @@ public class User {
     public User (User u)
     {
         this.studentnumber = u.studentnumber;
+        this.name = u.name;
+        this.id = u.id;
+        this.pw = pw;
+    }
+
+    public User()
+    {
+        this.studentnumber = null;
+        this.name = null;
+        this.id = null;
+        this.pw = null;
+    }
+
+    public User(String sn, String na, String i, String p)
+    {
+        this.studentnumber = sn;
+        this.name = na;
+        this.id = i;
+        this.pw = p;
     }
 
 }
