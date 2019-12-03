51c2f51cd49bd943d41981e31ee9a44a3cf533cc
donghyeon <kevink3@naver.com>
2019-12-03 20:24:23 +0900

    1130

--- app/src/main/java/com/example/myapplication/User.java
+++ app/src/main/java/com/example/myapplication/User.java
@@ -1,4 +1,15 @@
 package com.example.myapplication;
 
 public class User {
+
+    public String studentnumber;
+    public String name;
+    public String id;
+    public String pw;
+
+    public User (User u)
+    {
+        this.studentnumber = u.studentnumber;
+    }
+
 }
