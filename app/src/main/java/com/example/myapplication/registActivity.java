21698973d7aec03327bdb2bb4b34dead2753fdd0
yoonjin-choi <yoonjin825@gmail.com>
2019-12-07 15:37:37 +0900

    subactivity

--- app/src/main/java/com/example/myapplication/registActivity.java
+++ app/src/main/java/com/example/myapplication/registActivity.java
@@ -2,13 +2,27 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class registActivity extends AppCompatActivity {
 
+    SQLiteDatabase UserDB;
+    EditText getName;
+    EditText getId;
+    EditText getNum;
+    EditText getPw;
+    String tableName1="user";
+    String tableName2="timetable";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_regist);
+
+        UserDB=openOrCreateDatabase("studentDB",MODE_PRIVATE,null);
+        UserDB.execSQL("create table if not exists "+tableName1+"("+"STUNUM VARCHAR, "+"NAME VARCHAR, "+"ID VARCHAR, "+"PASSWORD VARCHAR, "+"PRIMARY KEY (STUNUM))");
+
+
     }
 }
