22862381dd5662cb654aed9cc6f33fb238c771c2
donghyeon <kevink3@naver.com>
2019-12-14 23:03:32 +0900

    day

--- app/src/main/java/com/example/myapplication/map.java
+++ app/src/main/java/com/example/myapplication/map.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.Manifest;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,8 +21,11 @@ import android.location.Geocoder;
 import android.location.LocationManager;
 
 import android.content.pm.PackageManager;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import java.io.IOException;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
 import androidx.annotation.UiThread;
@@ -29,42 +33,88 @@ import androidx.annotation.UiThread;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.CameraUpdate;
 import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.overlay.Marker;
 
-public class map extends Fragment {
+public class map extends FragmentActivity implements OnMapReadyCallback {
 
     MapFragment mapFragment;
-
-    private GpsTracker gpsTracker;
-
-    private static final int GPS_ENABLE_REQUEST_CODE = 2001;
-    private static final int PERMISSIONS_REQUEST_CODE = 100;
-    String[] REQUIRED_PERMISSIONS  = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+    GpsTracker gp;
+    int day;
+    Calendar cal = Calendar.getInstance();
+    String korDay;
 
 
-    public map(){
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.showmap);
+
+        day = cal.get(Calendar.DAY_OF_WEEK);
+        switch (day){
+            case 1:
+                korDay = "일";
+                break;
+            case 2:
+                korDay = "월";
+                break;
+            case 3:
+                korDay = "화";
+                break;
+            case 4:
+                korDay = "수";
+                break;
+            case 5:
+                korDay = "목";
+                break;
+            case 6:
+                korDay = "금";
+                break;
+            case 7:
+                korDay = "토";
+                break;
+        }
+
+        System.out.println(korDay);
+        //MainActivity.UserDB
+        gp = new GpsTracker(this);
+        mapFragment = (MapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+
+        if (mapFragment == null)
+        {
+            mapFragment = MapFragment.newInstance();
+            getSupportFragmentManager().beginTransaction().add(R.id.map, mapFragment).commit();
+        }
+        mapFragment.getMapAsync(this);
 
     }
 
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.showmap, container, false);
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        naverMap.setMapType(NaverMap.MapType.Navi);
 
+        naverMap.setSymbolScale(1.0f);
+        LatLng curlo = new LatLng(  gp.getLatitude(), gp.getLongitude() );
 
-        mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        CameraUpdate cameraUpdate1 = CameraUpdate.scrollTo(curlo);
+        naverMap.moveCamera(cameraUpdate1);
 
+        CameraUpdate cameraUpdate2 = CameraUpdate.zoomTo(16);
+        naverMap.moveCamera(cameraUpdate2);
 
+        Marker marker1 = new Marker();
+        marker1.setPosition(curlo);
+        marker1.setMap(naverMap);
 
-
-
-
-
-
-        return view;
+        marker1.setSubCaptionText("현위치");
+        marker1.setSubCaptionColor(Color.GREEN);
+        marker1.setSubCaptionHaloColor(Color.YELLOW);
+        marker1.setSubCaptionTextSize(10);
     }
-
-
 }
--- app/src/main/java/com/example/myapplication/nav.java
+++ app/src/main/java/com/example/myapplication/nav.java
@@ -2,14 +2,11 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
-
 import com.example.myapplication.ModifyTable;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
-
 import android.view.MenuItem;
 import android.view.View;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -18,14 +15,10 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-
 import com.google.android.material.navigation.NavigationView;
-
 import androidx.drawerlayout.widget.DrawerLayout;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
 import android.view.Menu;
 
 public class nav extends AppCompatActivity {
@@ -33,11 +26,13 @@ public class nav extends AppCompatActivity {
     private AppBarConfiguration mAppBarConfiguration;
     private FragmentManager fm;
     private FragmentTransaction ft;
-    Fragment fr = new map();
+    Fragment fr = new ModifyTable();
     DrawerLayout drawer;
     NavigationView navigationView;
     Toolbar toolbar;
 
+    Intent intent2;
+    int flag ;
 
 
 
@@ -46,8 +41,10 @@ public class nav extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nav);
         toolbar = findViewById(R.id.toolbar);
+        intent2 = new Intent(getApplicationContext(),  map.class);
+        flag = 1;
 
-        toolbar.setTitle("지도");
+        toolbar.setTitle("시간표");
         getSupportFragmentManager().beginTransaction().add(R.id.fra, fr).commit();
 
 
@@ -75,7 +72,6 @@ public class nav extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
 
-
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -84,26 +80,23 @@ public class nav extends AppCompatActivity {
                 switch (item.getItemId())
                 {
                     case R.id.map:
-                        fr = new map();
-                        toolbar.setTitle("지도");
+                        startActivity(intent2);
+                        //startActivity(intent2);
+                        flag = 0;
+                        //fr = new map();
+                        //toolbar.setTitle("지도");
                         break;
 
                     case R.id.modifytimetable:
                         fr = new ModifyTable();
                         toolbar.setTitle("시간표 수정");
                         break;
-
-
-
                 }
                 fm = getSupportFragmentManager();
                 ft = fm.beginTransaction();
                 ft.replace(R.id.fra, fr);
                 ft.addToBackStack(null);
                 ft.commit();
-
-
-
 
                 return true;
                 //return false;
@@ -124,5 +117,20 @@ public class nav extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+
+    public void mapmap()
+    {
+        startActivity( intent2 );
+        /*if( flag == 0)
+        {
+            startActivity( intent2 );
+            flag = 1;
+        }*/
+        /*else if (flag == 1)
+        {
+
+        }*/
     }
 }
